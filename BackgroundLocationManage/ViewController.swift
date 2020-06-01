0146c86a7f6aeb03790605ed38be0bf8e69cebda
u39ueda <ueda.u39@gmail.com>
2020-06-01 21:59:26 +0900

    Initial Commit

---
+++ BackgroundLocationManage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundLocationManage
+//
+//  Created by 植田裕作 on 2020/06/01.
+//  Copyright © 2020 Yusaku Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BackgroundLocationManageTests/BackgroundLocationManageTests.swift
@@ -0,0 +1,34 @@
+//
+//  BackgroundLocationManageTests.swift
+//  BackgroundLocationManageTests
+//
+//  Created by 植田裕作 on 2020/06/01.
+//  Copyright © 2020 Yusaku Ueda. All rights reserved.
+//
+
+import XCTest
+@testable import BackgroundLocationManage
+
+class BackgroundLocationManageTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
