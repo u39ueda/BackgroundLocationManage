061fd33345f3ff2ddc70ec261c62d57d99d3a5bd
u39ueda <ueda.u39@gmail.com>
2020-06-01 22:25:07 +0900

    Create ViewControllers

---
+++ BackgroundLocationManage/Map/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  BackgroundLocationManage
+//
+//  Created by 植田裕作 on 2020/06/01.
+//  Copyright © 2020 Yusaku Ueda. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
